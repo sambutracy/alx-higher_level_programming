1acc940ebec08d938558dccba1b22836c25f4ffe
sambutracy <tracysambut@gmail.com>
2024-01-08 05:38:36 -0800

    task 7: Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         space = ""
         for element in row:
-            print("{}{:d} ".format(element), end="")
+            print("{}{:d}".format(space, element), end="")
             space = " "
         print('')
