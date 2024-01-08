4ea74cb951b28786b22d5424e6a52a6b79116ee8
sambutracy <tracysambut@gmail.com>
2024-01-08 05:51:53 -0800

    task 9: more returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a(), tuple_b()):
+    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
+    # Ensure each tuple has at least 2 elements, filling with 0 if needed
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    # Sum the corresponding elements of the tuples
+    return result
