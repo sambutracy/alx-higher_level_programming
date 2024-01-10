c20f5f12eb6656d4503a90b4099c2721c61927d9
sambutracy <tracysambut@gmail.com>
2024-01-10 15:43:38 -0800

    Task 15: Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    # Using list comprehension to create a new dictionary
     new_dict = {key: val for key, val in a_dictionary.items() if val != value}
     return new_dict
