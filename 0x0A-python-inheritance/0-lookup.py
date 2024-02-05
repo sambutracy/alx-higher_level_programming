38767e74b786269c44100b142d7c778a5654d53b
sambutracy <tracysambut@gmail.com>
2024-02-05 15:47:06 -0800

    task 0- lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Class that returns directory and methods """
 
+
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
-
