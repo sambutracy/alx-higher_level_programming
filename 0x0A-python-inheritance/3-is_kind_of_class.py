6eb3aa68c894f9fa1048c60d20919388d59b4932
sambutracy <tracysambut@gmail.com>
2024-02-05 16:04:40 -0800

    task 3: same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function to check the class and inheritance of an object"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if the object is an instance or inherited from the specified class."""
+    return isinstance(obj, a_class)
