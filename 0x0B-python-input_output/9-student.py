5f243f7fe22a9499ad8bb576b3628e4201d32a01
sambutracy <tracysambut@gmail.com>
2024-02-07 14:15:56 -0800

    task 9- student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines the student's class"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieve a dictionary representation of a Student instance"""
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
