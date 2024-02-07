b4cb46fcc6a04f401a9f1c3c2be59bde951669ba
sambutracy <tracysambut@gmail.com>
2024-02-06 16:10:16 -0800

    task 6: creates an object from ajson file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """creates an object from a json file"""
-
 import json
+
 
 def load_from_json_file(filename):
     """creates an object from a json file"""
-    with open(filename, mode='r', encoding='utr-8') as file:
-        return json.load(file)  
+    with open(filename, 'r')as json_file:
+        temp=json.loads(json_file.read())
+    return temp
