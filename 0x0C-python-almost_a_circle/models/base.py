57d04011c1df9bcc21422ba6269bd2a8cfd41d60
sambutracy <tracysambut@gmail.com>
2024-02-12 14:35:07 -0800

    tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,59 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Converts a list of dictionaries to a JSON string.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: The JSON string representation of the list of dictionaries.
+        """
+        if len(list_dictionaries) == 0 or \
+                list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + '.json'
+        if list_objs is None:
+            list_dict = []
+        else:
+            list_dict = [obj.to_dictionary() for obj in list_objs]
+            json_string = cls.to_json_string(list_dict)
+
+        with open(filename, 'w') as f:
+            f.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "[]":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Square":
+                dummy = cls(1)
+            elif cls.__name__ == "Rectangle":
+                dummy = cls(1, 1)
+            dummy.update(**dictionary)
+            return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + '.json'
+        try:
+            with open(cls.__name__ + ".json", "r") as file:
+                list_dicts = Base.from_json_string(file.read())
+                return [cls.create(**dic) for dic in list_dicts]
+        except FileNotFoundError:
+            return []
