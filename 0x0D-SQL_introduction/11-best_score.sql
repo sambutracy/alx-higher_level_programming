795b5e3a4cbdf05af41ca91f06cd627b9dbbfcc1
sambutracy <tracysambut@gmail.com>
2024-03-12 16:25:52 -0700

    task 11:select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- List all records with a score >= 10 in the table second_table, displaying score and name, ordered by score (top first)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
