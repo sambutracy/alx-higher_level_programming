857d35d455f8bac377803bd6785fc5bc50fadc33
sambutracy <tracysambut@gmail.com>
2024-03-13 13:46:29 -0700

    task 0: privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This script lists the privileges of specified MySQL users.
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
