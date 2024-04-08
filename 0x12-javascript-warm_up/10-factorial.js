ba07ced9433d56ddbead9422b17094c1b1d5c8b1
sambutracy <tracysambut@gmail.com>
2024-04-08 16:20:45 -0700

    Add task 10: factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Filename: 10-factorial.js
+
+'use strict';
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+
+console.log(factorial(num));
