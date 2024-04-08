29cc6b780bf897fbb896b16a0724c5923a474be7
sambutracy <tracysambut@gmail.com>
2024-04-08 16:04:06 -0700

    Add task 6:Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Filename: 6-multi_languages_loop.js
+
+'use strict';
+
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (const line of lines) {
+  console.log(line);
+}
