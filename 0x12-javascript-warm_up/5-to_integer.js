cfc1ff5da9cb982939f004103dce60cb419785e2
sambutracy <tracysambut@gmail.com>
2024-04-08 16:02:37 -0700

    Add task 5: An integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Filename: 5-to_integer.js
+
+'use strict';
+
+const num = parseInt(process.argv[2]);
+
+if (!isNaN(num)) {
+  console.log('My number:', num);
+} else {
+  console.log('Not a number');
+}
