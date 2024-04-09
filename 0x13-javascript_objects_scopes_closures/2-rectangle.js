c8aa469238e5ada652ec575fd3752aea1dabc435
sambutracy <tracysambut@gmai.com>
2024-04-09 15:57:21 -0700

    add task 2: rectangle 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Filename: 2-rectangle.js
+
+'use strict';
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
