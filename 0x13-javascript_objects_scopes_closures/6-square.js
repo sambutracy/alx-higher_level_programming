a8480e9cd4c3f41da7438f58356734547c4b35e8
sambutracy <tracysambut@gmai.com>
2024-04-09 16:31:42 -0700

    add task 6:squares

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// Filename: 6-square.js
+
+'use strict';
+
+const OldSquare = require('./5-square');
+
+class Square extends OldSquare {
+  constructor(size) {
+    super(size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
+
