24966e78e1ebd4b371a48f540f4b497a453a123b
sambutracy <tracysambut@gmail.com>
2024-04-18 14:10:52 -0700

    task 0: fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
+"""This module defines a python program that lists all states from the database
+hbtn_0e_0_usa.
+"""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Check if the number of arguments is correct
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Extracting arguments
+if __name__ == '__main__':
+    """
+    Getting access to the database and get the states
+    """
     username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    dbname = sys.argv[3]
+    connect = MySQLdb.connect(host="localhost", user=username,
+            passwd=password, db=dbname)
+    cursor = connect.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY id ASC')
+    result = cursor.fetchall()
 
-    try:
-        # Connect to MySQL server running on localhost at port 3306
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-
-        # Create a cursor object using cursor() method
-        cursor = db.cursor()
-
-        # Execute SQL query to select all states from the states table
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-        # Fetch all the rows using fetchall() method
-        states = cursor.fetchall()
-
-        # Print the results
-        for state in states:
-            print(state)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-    finally:
-        # Close the database connection
-        if 'db' in locals() or 'db' in globals():
-            db.close()
+    for row in result:
+        print(row)
