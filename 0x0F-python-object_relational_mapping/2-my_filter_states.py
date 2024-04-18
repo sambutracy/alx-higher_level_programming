8ea24017cfaa9a1074baa36b29a3b5f00d799fa1
sambutracy <tracysambut@gmail.com>
2024-04-18 15:39:33 -0700

    task 2: filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,27 +7,31 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if correct number of arguments is provided
+    """ Check if correct number of arguments is provided"""
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>"
+                .format(sys.argv[0]))
         sys.exit(1)
 
-    # Extract command-line arguments
+    """Extract command-line arguments"""
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to MySQL server
+    """ Connect to MySQL server"""
     conn = MySQLdb.connect(host='localhost', user=username,
                            passwd=password, db=dbname)
     cur = conn.cursor()
 
-    # Execute SQL query to select states based on user input
+    """ Execute SQL query to select states based on user input"""
     stmt = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cur.execute(stmt, (state_name,))
 
-    # Fetch and print results
+    """ Fetch and print results"""
     result = cur.fetchall()
     for row in result:
         print(row)
+
+    """ Close database connection"""
+    conn.close()
