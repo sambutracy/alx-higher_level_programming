74d025fc0ad7fde253046e218d5ca0da0edf378b
sambutracy <tracysambut@gmail.com>
2024-04-18 16:21:58 -0700

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines the City model for the cities table in the database.
+
+This script defines a SQLAlchemy model for the City class, which represents
+the cities table in the database. It has columns for id, name, and state_id
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Represents a city in the database.
+
+    Attributes:
+        id (int): The unique identifier for the city.
+        name (str): The name of the city.
+        state_id (int): The foreign key referencing the id of the state
+    """
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
