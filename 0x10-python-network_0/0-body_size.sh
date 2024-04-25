2834f79d4b69cf6ea1f5bb6834207c14c90ad95a
sambutracy <tracysambut@gmail.com>
2024-04-25 13:43:38 -0700

    Add task 1:cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# This script takes a URL as input, sends a request,
-#and displays the size of the response body in bytes
-
-#Send request to provided URL and display the size of response body in bytes
+#This script displays the size of the response in bytes
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
 
