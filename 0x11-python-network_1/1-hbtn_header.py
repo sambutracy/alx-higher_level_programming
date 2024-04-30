525f032d302783d2c45425e62113b27b32d5ef14
sambutracy <tracysambut@gmail.com>
2024-04-30 07:24:28 -0700

    task 1:Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Script that takes in a URL as an argument,
+sends a request to the URL using urllib
+"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        headers = response.headers
+        x_request_id = headers.get('X-Request-Id')
+        print(x_request_id)
