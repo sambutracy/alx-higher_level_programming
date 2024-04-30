49919a6d7a45e16d53539120fd9a290cd073c311
sambutracy <tracysambut@gmail.com>
2024-04-30 07:12:32 -0700

    fix task 0: What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,31 +5,10 @@ Module to fetch and display status from a URL using urllib
 
 import urllib.request
 
-def fetch_url_status(url):
-    """
-    Fetches the status of a URL using urllib
-
-    Args:
-        url (str): The URL to fetch status from
-
-    Returns:
-        str: The status information
-    """
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        utf8_content = content.decode('utf-8')
-        return utf8_content
-
-def display_status():
-    """
-    Displays the status of the URL
-    """
-    url = "https://alx-intranet.hbtn.io/status"
-    body_response = fetch_url_status(url)
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    data = response.read()
     print("Body response:")
-    print("    - type:", type(body_response))
-    print("    - content:", repr(body_response))
-    print("    - utf8 content:", body_response)
-
-if __name__ == "__main__":
-    display_status()
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+    print("\t- utf8 content:", data.decode('utf-8'))
